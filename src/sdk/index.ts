1a06faa6e26f1b9b492d81d70a119a942f0b1c27
Ricardo <ricardo.lcmr@gmail.com>
2020-09-21 11:50:12 +0100

    Created Client SDK For available Endpoints

---
+++ src/sdk/index.ts
@@ -0,0 +1,23 @@
+import { ITweet, IUser } from "./../../interfaces/ITweet";
+
+const getTimelineForUser = (user_id: string): Promise<ITweet[]> =>
+  new Promise((resolve, reject) => {
+    fetch(`/api/tweets/${encodeURIComponent(user_id)}`)
+      .then((r: Response) => r.json())
+      .then((r: ITweet[]) => resolve(r))
+      .catch((e: Error) => {
+        reject(e);
+      });
+  });
+  
+const getUsersForScreenName = (screen_name: string): Promise<IUser[]> =>
+  new Promise((resolve, reject) => {
+    fetch(`/api/twitter/user/${encodeURIComponent(screen_name)}`)
+      .then((r: Response) => r.json())
+      .then((r: IUser[]) => resolve(r))
+      .catch((e: Error) => {
+        reject(e);
+      });
+  });
+
+  export {getTimelineForUser,getUsersForScreenName}
